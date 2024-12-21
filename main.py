04f45ecd03bd5bc88cfab7faa2adb28b15f62930
daniellucena1 <daniellucena.jt@gmail.com>
2024-12-21 19:34:22 -0300

    criando a imagem do espectrograma

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ audio_array, taxa_hz = librosa.load(audio_path, sr=None)
 
 espectograma = librosa.amplitude_to_db(np.abs(librosa.stft(audio_array)), ref=np.max)
 
+figura, ax = plt.subplots(figsize=(10, 5))
+librosa.display.specshow(espectograma, sr=taxa_hz, x_axis='time', y_axis='hz', cmap='magma', ax=ax)
+ax.axis('off') 
+
+espectograma_salvo = "spectrogram.png"
+plt.savefig(espectograma_salvo, bbox_inches='tight', pad_inches=0, dpi=300)
+plt.close()
+
