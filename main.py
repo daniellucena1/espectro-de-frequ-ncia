ff34afc902a1c46c69a05f390ae0d387c6e12a2e
daniellucena1 <daniellucena.jt@gmail.com>
2024-12-21 19:30:27 -0300

    importando a música e criando o espectograma

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import librosa
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 
+# Carregar o áudio
+audio_path = "something.mp3"  # Substitua pelo caminho do áudio
+audio_array, taxa_hz = librosa.load(audio_path, sr=None)
+
+espectograma = librosa.amplitude_to_db(np.abs(librosa.stft(audio_array)), ref=np.max)
 
